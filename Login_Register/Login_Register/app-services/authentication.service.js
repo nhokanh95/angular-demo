cd2a1d0c8df1900ca2a3afb6677130fe82265352
nhokanh95 <daibang321@gmail.com>
2016-11-01 23:51:08 +0700

    Desktop

---
+++ Login_Register/Login_Register/app-services/authentication.service.js
@@ -0,0 +1,5 @@
+﻿/// <reference path="E:\Github\angular-demo\Login_Register\Login_Register\Scripts/angular.js" />
+angular
+    .module('app')
+.factory('AuthenticationService', AuthenticationService);
+AuthenticationService.$inject=['$http','$cookieStore','$rootScope','$timeout','UserService']
---
+++ Login_Register/Login_Register/app-services/user.service.js
@@ -0,0 +1,47 @@
+﻿/// <reference path="E:\Github\angular-demo\Login_Register\Login_Register\Scripts/angular.js" />
+angular
+    .module('app')
+.factory('UserService', UserService);
+UserService.$inject = ['$http'];
+function UserService($http) {
+    var service = {};
+    service.GetAll = GetAll;
+    service.GetById = GetById;
+    service.GetByUserName = GetByUserName;
+    service.Create = Create;
+    service.Update = Update;
+    service.Delete = Delete;
+    return service;
+    function GetAll() {
+        return $http.get('/api/users').then(handleSuccess, handleError('Error getting all users'));
+    }
+    function GetById(id) {
+        return $http.get('/api/users/' + id).then(handleSuccess, handleError('Error getting user by id'));
+    }
+    function GetByUserName(username) {
+        return $http.get('/api/users/' + username).then(handleSuccess, handleError('Error getting user by username'));
+    }
+    function Create(user) {
+        return $http.post('/api/users', user).then(handleSuccess, handleError('Error creating user'));
+    }
+
+    function Update(user) {
+        return $http.put('/api/users/' + user.id, user).then(handleSuccess, handleError('Error updating user'));
+    }
+
+    function Delete(id) {
+        return $http.delete('/api/users/' + id).then(handleSuccess, handleError('Error deleting user'));
+    }
+
+    // private functions
+
+    function handleSuccess(res) {
+        return res.data;
+    }
+
+    function handleError(error) {
+        return function () {
+            return { success: false, message: error };
+        };
+    }
+}
